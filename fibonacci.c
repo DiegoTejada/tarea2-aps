73eb67cf3b86e425c7912d799a7282478dc8dd23
Diego Tejada <31262863+DiegoTejada@users.noreply.github.com>
2019-08-19 12:34:06 -0500

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -6,6 +6,9 @@ int main(){
     printf("Numero");
     scanf("%d",n);
     printf("0 1 ");
+    for(k=2,k<=n,k++)
+        printf("&d&c",f(k),(k%10=0 ? '\n':' '));
+    return 0;
 }
 int f(int n){
   
